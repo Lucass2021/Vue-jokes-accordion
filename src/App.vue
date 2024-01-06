9c1e2f07ddc36455822c43898af91cbb2e49ac3b
Lucas Dias <lucas.allx@hotmail.com>
2024-01-06 10:05:16 -0300

    initial card component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>
-    Hello World
+  <div class="container">
+    <div class="bg-img-container">
+      <img class="bg-img" src="./assets/background-pattern-desktop.svg" alt="">
+    </div>
+    <div class="card-container">
+      <CardComponent/>
+    </div>
   </div>
 </template>
 
 <script setup>
+import CardComponent from "./components/CardComponent.vue";
 
 </script>
 
 <style>
+*{
+  margin:0px;
+  padding:0px;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+}
+
+body{
+  background: #F9EEFF;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.bg-img-container{
+  position: absolute;
+  top:0;
+  z-index: -1;
+  width:100%;
+}
+
+.bg-img{
+  width:100%;
+}
 
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Developers Joke</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+div{
+    background: #fff;
+    padding:30px;
+    width: 600px;
+    margin:0 auto;
+    border-radius:10px;
+}
+
+div h1{
+    font-size: 40px;
+    text-align: center;
+}
+
+</style>
