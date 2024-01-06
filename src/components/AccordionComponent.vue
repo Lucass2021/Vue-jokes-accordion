4a658b3210bb45498f345b1bd91cda83af65bb28
Lucas Dias <lucas.allx@hotmail.com>
2024-01-06 11:55:58 -0300

    desktop style

--- src/components/AccordionComponent.vue
+++ src/components/AccordionComponent.vue
@@ -1,48 +1,59 @@
 <template>
     <div class="q-pa-md">
-        <q-list bordered class="rounded-borders">
-        <q-expansion-item expand-separator label="Account settings">
+      <q-list bordered class="rounded-borders">
+        <q-expansion-item expand-separator v-for="joke in jokeData" :key="joke.setup" :label="joke.setup" class="accordion-title">
             <q-card>
             <q-card-section>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                eveniet doloribus ullam aliquid.
+                <p class="accordion-text">
+                    {{ joke.delivery }}
+                </p>
             </q-card-section>
-            </q-card>
+          </q-card>
         </q-expansion-item>
-        <hr>
-        <q-expansion-item expand-separator label="Account settings">
-            <q-card>
-            <q-card-section>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                eveniet doloribus ullam aliquid.
-            </q-card-section>
-            </q-card>
-        </q-expansion-item>
-        <hr>
-        <q-expansion-item expand-separator label="Account settings">
-            <q-card>
-            <q-card-section>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                eveniet doloribus ullam aliquid.
-            </q-card-section>
-            </q-card>
-        </q-expansion-item>
-        </q-list>
+      </q-list>
     </div>
 </template>
-
+  
 <script setup>
+    import { ref, onMounted } from "vue";
+
+    let jokeData = ref([]);
+
+    const handleJokeApiRequest = async () => {
+        try {
+            const req = await fetch(`https://v2.jokeapi.dev/joke/Programming?type=twopart&amount=5`);
+            const json = await req.json();
+            jokeData.value = json.jokes;
+        } catch (error) {
+            console.error("API ERROR", error);
+        }
+    };
+
+    onMounted(() => {
+        console.log("-Running API");
+        handleJokeApiRequest();
+        console.log(jokeData)
+    });
 
 </script>
+  
 
 <style scoped>
 
 hr{
     color: #eee;
     opacity: 0.3;
+}
+
+.accordion-title{
+    color: #301534;
+    font-weight: 700;
+}
+
+.accordion-text{
+    color: #686469;
+    font-weight: 400;
+    margin-bottom: 0px;
 }
 
 
